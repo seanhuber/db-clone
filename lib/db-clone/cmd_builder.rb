f2a3cd7deb13752aadd3caae0cf8af989b6c2a67
Sean Huber <seanhuber@seanhuber.com>
2016-05-30 00:13:16 -0500

    bug: ignore_tables using destination instead of source

--- lib/db-clone/cmd_builder.rb
+++ lib/db-clone/cmd_builder.rb
@@ -27,7 +27,7 @@ module DbClone
         "--port=#{src_dest[:src]['port']}"
       ]
 
-      DbClone.config[:ignore_tables].each{|tbl| mysqldump_args << "--ignore-table=#{src_dest[:dest]['database']}.#{tbl}"} if DbClone.config && DbClone.config[:ignore_tables].is_a?(Array)
+      DbClone.config[:ignore_tables].each{|tbl| mysqldump_args << "--ignore-table=#{src_dest[:src]['database']}.#{tbl}"} if DbClone.config && DbClone.config[:ignore_tables].is_a?(Array)
 
       (mysqldump_args + [
         "#{src_dest[:src]['database']}",
