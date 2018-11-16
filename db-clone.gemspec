af78d6b90e686e46ab3127af2d46ca531e645c64
Chris Hall <chall8908@gmail.com>
2018-11-16 11:14:30 -0600

    Replace old prompts with fancy CLI::UI prompts

--- db-clone.gemspec
+++ db-clone.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ['lib']
 
   spec.add_dependency 'colorize', '~> 0.7'
+  spec.add_dependency 'cli-ui', '~> 1.2.1'
 
   spec.add_development_dependency 'bundler', '~> 1.15'
   spec.add_development_dependency 'rake', '~> 10.0'
--- lib/db/clone/cmd_prompt.rb
+++ lib/db/clone/cmd_prompt.rb
@@ -1,44 +1,39 @@
+require 'cli/ui'
+
+CLI::UI::StdoutRouter.enable
+
 module Db
   module Clone
     module CmdPrompt
-      # def ask_for_text instruction, default_text=nil
-      #   if default_text
-      #     print "\n#{instruction} [default = \"#{default_text}\"]: "
-      #   else
-      #     print "\n#{instruction}: "
-      #   end
-      #   text = STDIN.gets.chomp.strip
-      #   text = default_text if text.blank? && default_text
-      #   text
-      # end
+      def frame(title)
+        CLI::UI::Frame.open(title) do
+          yield
+        end
+      end
 
-      def ask_yes_no question, default_to_yes=true
-        print "\n#{question} [#{default_to_yes ? 'Yn' : 'yN'}]: "
-        answer = STDIN.gets.chomp.strip.downcase
-        default_to_yes ? !(answer == 'n') : answer == 'y'
+      def ask_yes_no question, default_to_yes: true
+        if default_to_yes
+          CLI::UI.confirm question
+        else
+          CLI::UI.ask(question, options: %w(no yes)) == 'yes'
+        end
       end
 
       def prompt question, default, options={}
-        default_num = nil
-        numbered_opts = options.map.with_index do |opt, idx|
-          default_num = idx+1 if opt[0] == default
-          [idx+1, {ret_key: opt[0], label: opt[1]}]
-        end.to_h
+        opts = options.except(default)
+        default_option = options[default]
 
-        puts "\n"
-        numbered_opts.each{|choice_num, opt| puts "  [#{choice_num.to_s.white}] #{opt[:label]}" }
+        CLI::UI::Prompt.ask(question) do |handler|
+          handler.option(default_option) { default }
 
-        print "\n#{question} [1-#{options.length}, default=#{default_num}]: "
-
-        selection = STDIN.gets.chomp.strip.to_i
-        selection = default_num if selection.zero?
-
-        if numbered_opts.has_key? selection
-          numbered_opts[selection][:ret_key]
-        else
-          puts "#{'Invalid selection:'.red} #{selection}"
-          prompt question, default, options
+          opts.each do |(value, text)|
+            handler.option(text.to_s) { value }
+          end
         end
+      end
+
+      def fmt text
+        CLI::UI.fmt text
       end
     end
   end
